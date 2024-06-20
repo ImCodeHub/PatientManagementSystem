dd924b99752b90620a41caa010d590518b47c051
Ankit <ankitsharma.as420@gmail.com>
2024-06-20 23:30:45 +0530

    created service implementation

---
+++ PatientManagementSystem/src/main/java/com/Patient/PatientManagementSystem/Service/implement/PatientServiceImpl.java
@@ -0,0 +1,97 @@
+package com.Patient.PatientManagementSystem.Service.implement;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Patient.PatientManagementSystem.Entity.Patient;
+import com.Patient.PatientManagementSystem.Model.PatientModel;
+import com.Patient.PatientManagementSystem.Repository.PatientRepository;
+import com.Patient.PatientManagementSystem.Service.PatientService;
+
+@Service
+public class PatientServiceImpl implements PatientService {
+
+    Logger logger = LoggerFactory.getLogger(PatientServiceImpl.class);
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    @Override
+    public String savePatient(Patient patient) {
+        logger.info("patient saved {}", patient);
+        patientRepository.save(patient);
+        return "patient saved successfully.";
+    }
+
+    @Override
+    public List<PatientModel> getAllPatient() {
+        List<PatientModel> list = new ArrayList<>();
+        List<Patient> patients = patientRepository.findAll();
+        if (!patients.isEmpty()) {
+
+            for (Patient patient : patients) {
+                PatientModel patientModel = new PatientModel();
+
+                patientModel.setId(patient.getId());
+                patientModel.setFirstName(patient.getFirstName());
+                patientModel.setLastName(patient.getLastName());
+                patientModel.setAddress(patient.getAddress());
+
+                list.add(patientModel);
+            }
+            return list;
+        }
+        throw new UnsupportedOperationException("Unimplemented method 'getAllPatient'");
+    }
+
+    @Override
+    public List<PatientModel> getPatient(Long id) {
+        List<PatientModel> list = new ArrayList<>();
+        Optional<Patient> optional = patientRepository.findById(id);
+        if (optional.isPresent()) {
+            Patient patient = optional.get();
+            PatientModel patientModel = new PatientModel();
+            patientModel.setId(patient.getId());
+            patientModel.setFirstName(patient.getFirstName());
+            list.add(patientModel);
+            return list;
+        } else {
+
+            throw new UnsupportedOperationException("Unimplemented method 'getPatient'");
+        }
+    }
+
+    @Override
+    public Boolean updatePatient(Long id, PatientModel patientModel) {
+        Optional<Patient> optional = patientRepository.findById(id);
+        if (optional.isPresent()) {
+            Patient patient = optional.get();
+            logger.info("patient change {}", patientModel.getFirstName());
+
+            patient.setFirstName(patientModel.getFirstName());
+            patientRepository.save(patient);
+            return true;
+        } else {
+            throw new UnsupportedOperationException("Unimplemented method 'updatePatient'");
+        }
+    }
+
+    @Override
+    public Boolean deletePatient(Long id) {
+        Optional<Patient> optional = patientRepository.findById(id);
+        if (optional.isPresent()) {
+            logger.info("patient has been deleted for this id -{}",id);
+            patientRepository.deleteById(id);
+            return true;
+        } else {
+            throw new UnsupportedOperationException("Unimplemented method 'deletePatient'");
+        }
+    }
+
+}
