99f99177b77f42f883cda0b1e0189d4ef68b82ae
Ankit <ankitsharma.as420@gmail.com>
2024-06-22 00:34:27 +0530

    created new Hibernate JPA method to get email from db

--- PatientManagementSystem/src/main/java/com/Patient/PatientManagementSystem/Repository/PatientRepository.java
+++ PatientManagementSystem/src/main/java/com/Patient/PatientManagementSystem/Repository/PatientRepository.java
@@ -1,5 +1,7 @@
 package com.Patient.PatientManagementSystem.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.Patient.PatientManagementSystem.Entity.Patient;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Optional<Patient> findByEmail(String email);
 
 }
