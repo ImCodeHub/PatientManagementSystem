4183965f07d57acb7f33e80e0f939548503e7fbc
Ankit <ankitsharma.as420@gmail.com>
2024-06-21 23:30:46 +0530

    added jsonformat for local date

--- PatientManagementSystem/src/main/java/com/Patient/PatientManagementSystem/Model/PatientModel.java
+++ PatientManagementSystem/src/main/java/com/Patient/PatientManagementSystem/Model/PatientModel.java
@@ -2,6 +2,7 @@ package com.Patient.PatientManagementSystem.Model;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.AllArgsConstructor;
@@ -16,7 +17,10 @@ public class PatientModel {
     private Long id;
     private String firstName;
     private String lastName;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private LocalDate dateOfBirth;
+
     private String gender;
     private String contactNumber;
     private String email;
